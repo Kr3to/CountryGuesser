9e2f20029f32d39673dad0b1c5ea34b21e1b96ad
Kreto <mateusz.kret@zse.krakow.pl>
2022-07-03 17:48:52 +0200

    Gamemode choosing, going back

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+function classic() {
+    document.getElementById("survival").style.display = "none";
+    document.getElementById("speedrun").style.display = "none";
+    document.getElementById("test").style.display = "block";
+}
+
+function speedrun() {
+    document.getElementById("classic").style.display = "none";
+    document.getElementById("survival").style.display = "none";
+    document.getElementById("test1").style.display = "block";
+}
+
 function survival() {
     document.getElementById("classic").style.display = "none";
     document.getElementById("speedrun").style.display = "none";
+    document.getElementById("test2").style.display = "block";
+}
+
+function back() {
+    document.getElementById("survival").style.display = "inline";
+    document.getElementById("speedrun").style.display = "inline";
+    document.getElementById("classic").style.display = "inline";
+    document.getElementById("test").style.display = "none";
+    document.getElementById("test1").style.display = "none";
+    document.getElementById("test2").style.display = "none";
 }
